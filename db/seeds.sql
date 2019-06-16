b438ee4a72eb667fa54a2d85d7cd26f18b6169fb
Michael Clemons <mwclemons@gmail.com>
2019-06-15 22:58:15 -0500

    missing logic and formatting

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Whopper', False);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double Double', False);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Quarter Pounder', False);
