ba0e130d67c2803b7ee43f0eadcf60b5c08aaeb9
Michael Clemons <mwclemons@gmail.com>
2019-06-18 10:20:23 -0500

    removed dirname

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.static("public"));
 // Parse request body as JSON
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static(path.join(__dirname, '/public')));
+// app.use(express.static(path.join(__dirname, '/public')));
 
 // Set Handlebars.
 var exphbs = require("express-handlebars");
