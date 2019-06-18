528012268eb90490763af2766b359e8a29f91d88
Michael Clemons <mwclemons@gmail.com>
2019-06-18 10:00:24 -0500

    fixed image path

--- config/connection.js
+++ config/connection.js
@@ -1,21 +1,21 @@
 var mysql = require("mysql");
 
-// var connection = mysql.createConnection({
-//   host: "localhost",
-//   port: 3306,
-//   user: "root",
-//   password: "snizz11bre",
-//   database: "burgers_db"
-// });
-
-
-
 var connection = mysql.createConnection({
-  host: "u0zbt18wwjva9e0v.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-  user: "fsqcmf435ga1u4z6",
-  password: "vfxfwvwdn13zlne7",
-  database: "r6wwld1m4y6jx070"
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "snizz11bre",
+  database: "burgers_db"
 });
+
+
+
+// var connection = mysql.createConnection({
+//   host: "u0zbt18wwjva9e0v.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+//   user: "fsqcmf435ga1u4z6",
+//   password: "vfxfwvwdn13zlne7",
+//   database: "r6wwld1m4y6jx070"
+// });
 
 
 connection.connect(function(err) {
